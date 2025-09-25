6975bb1f0a9a0b504d195a29cfbdb840cb2c5f23
Geraldine <c4dn5wld3@vossie.net>
2025-09-25 19:19:16 +0200

    practice using services

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -22,17 +22,11 @@ export class TasksComponent {
     return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(id: string) {}
-
   onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
-    this.isAddingTask = false;
-  }
-
-  onAddTask(taskData: NewTaskData) {
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 }
